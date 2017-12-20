43835807e54df5cfb63b933537fa33645e94d1b6
Karl 'NaN' Wikström <karlwik@student.chalmers.se>
2017-12-20 14:42:12 +0100

    Reformat code for day1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,37 +1,38 @@
 use std::io::{self, Read};
 
-fn main()
-{
+fn read_input() -> Vec<u8> {
 	let mut buffer: Vec<u8> = Vec::new();
 	io::stdin().read_to_end(&mut buffer).unwrap();
 
-	let input = buffer.iter()
+	buffer.iter()
 		// Filter out incorrect inputs
 		.filter(|&x| *x > 48 && *x <= 58)
 		// Convert ascii values to numbers
 		.map(|&x| x - 48)
-		.collect::<Vec<u8>>();
+		.collect::<Vec<u8>>()
+}
 
-	// Offset from one element to the element it will be compared to
-	let offset = input.len() / 2;
-	//let offset = 1;
-	
+fn sum_with_offset(input: &Vec<u8>, offset: usize) -> u32 {
 	let mut sum: u32 = 0;
-	for i in 0..input.len()
-	{
-		// Check if element matches the next one
-		if input[i] == input[(i + offset) % input.len()] 
-		{
-			println!(
-				"Found double. {} at {} equals {} at {}",
-				input[i],
-				i,
-				input[(i+1) % input.len()],
-				(i + offset) % input.len()
-			);
-
+	for i in 0..input.len() {
+		if input[i] == input[(i + offset) % input.len()] {
 			sum += input[i] as u32;
 		}
 	}
-	println!("Sum is {}", sum);
+	return sum;
+}
+
+fn part_1(input: &Vec<u8>) -> u32 {
+	sum_with_offset(input, 1)
+}
+
+fn part_2(input: &Vec<u8>, offset: usize) -> u32 {
+	sum_with_offset(input, offset)
+}
+
+fn main() {
+	let input: Vec<u8> = read_input();
+
+	println!("The sum for part 1 is: {}", part_1(&input));
+	println!("The sum for part 2 is: {}", part_2(&input, input.len() / 2));
 }
